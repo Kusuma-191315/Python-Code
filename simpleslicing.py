aa431e11c62aa67256632d0d27b16797a261cf8f
Kusuma <kusumayogesh290@gmail.com>
2026-03-04 17:13:55 +0530

    Initial commit

---
+++ simpleslicing.py
@@ -0,0 +1,24 @@
+s = "Kodnest"
+print(s[4])
+print(s[1])
+print(s[-3])
+print(s[-7])
+
+#using syntax of slicing
+print(s[2:3])
+print(s[2:2])
+
+#using slicing of 3 index syntax 
+#Positive stepcount(skiping alpha)
+print(s[1::2])
+print(s[1::2])
+print(s[1:5:2])
+print(s[::2])
+print(s[::1])#+1 and -1 means 0 so if start and end did'nt gave consider it as full and no skip
+
+#Negative stepcount
+print(s[-2::-1])
+print(s[::-1])
+print(s[-6::-1])
+print(s[-3:-6:-2])
+print(s[::-3])
